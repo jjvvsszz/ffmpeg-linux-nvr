00076fdbd568ea0bdaca34df3fa4cd9580765dad
Jão <jjvvsszz.gamer@gmail.com>
2024-06-15 11:06:05 -0300

    Update record.py
    change from openRTSP to FFmpeg

--- record.py
+++ record.py
@@ -1,5 +1,5 @@
 '''
-Simple recorder for cheap IP cameras using openRTSP
+Simple recorder for cheap IP cameras using ffmpeg
 Usage:
     Record camera1's footage in 600 second chunks
     python record.py --camera camera1 --record-period 600
@@ -12,23 +12,24 @@ import os
 import signal
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--camera", type=str, default="door",
+parser.add_argument("--camera", type=str, default="camera1",
                     help="Camera (camera1 or camera2)")
 parser.add_argument("--record-period", type=int, default=600,
                     help="Output file size in seconds")
 args = parser.parse_args()
 
-common =' -4 -B 10000000 -b 10000000 -f 20 -w 1920 -h 1080 -t -V' 
+common =' -vf scale=1280:-1 -c:v libx264 -preset superfast -crf 25 -c:a aac -b:a 128k'
+
 if args.camera == "camera1":
     # modify the IP address below to your camera1's IP
     # RTSP path might be different for each camera brand
     # also, modify the username and password
-    # default might be like admin:admin 
-    end = 'rtsp://username:password@192.168.1.2:554/11'
+    # default might be like "admin:admin" or "admin:"
+    cam = 'rtsp://username:password@ip:port/url'
 elif args.camera == "camera2":
-    end = 'rtsp://username:password@192.168.1.3:554/11'
+    cam = 'rtsp://username:password@ip:port/url'
 
-common = common + ' -d %d ' % args.record_period
+common = common + ' -t %d ' % args.record_period
 # Create the output directory
 outdir = './%s/' % args.camera
 os.system('mkdir -p %s' % outdir)
@@ -41,23 +42,16 @@ def return_filename():
     return fl
 
 while True:
+    start_time = time.time()
+
     filename = return_filename()
     outfile = './%s/%s.mp4' % (outdir, filename)
-    # Create the openRTSP command and its parameters
-    cmd = 'openRTSP ' + common + end 
+    # Create the ffmpeg command and its parameters
+    cmd = f'ffmpeg -rtsp_transport tcp -i ' + cam + common  + ' ' + outfile
     cmd = cmd.split(' ')
     cmd = [ix for ix in cmd if ix != '']
 
-    st = time.time()
-    with open(outfile,"wb") as outp:
-        proc = subprocess.Popen(cmd, shell=False,
-                                stdin=None, stdout=outp,
-                                stderr=None, close_fds=True)
-    time.sleep(args.record_period)
-    # Send the termination signal
-    print('Send termination signal')
-    proc.send_signal(signal.SIGHUP)
-    time.sleep(1)
-    os.kill(proc.pid, signal.SIGTERM) 
+    subprocess.run(cmd)
 
-    print('Elapsed %1.2f' % (time.time() - st))
+    elapsed = time.time() - start_time
+    print('Elapsed %1.2f' % (elapsed))
